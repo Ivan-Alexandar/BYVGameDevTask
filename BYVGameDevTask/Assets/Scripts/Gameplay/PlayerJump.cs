670e09dcf45b65342089b5f7dfdff84276752848
Ivan-Alexandar <ivalzlatkov@gmail.com>
2021-07-14 12:41:12 +0300

    Finally! A working jump animation

---
+++ BYVGameDevTask/Assets/Scripts/Gameplay/PlayerJump.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    private void Start()
+    {
+        Vector2 force = new Vector2(100, 200);
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        rb2d.AddForce(force, ForceMode2D.Force);
+    }
+
+}
