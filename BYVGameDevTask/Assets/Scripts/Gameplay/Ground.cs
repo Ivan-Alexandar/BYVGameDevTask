db4905a1ff59134d3d6713002cd8f2bfd46ce51f
Ivan-Alexandar <ivalzlatkov@gmail.com>
2021-07-15 17:20:40 +0300

    Working anim/double jump/bullet prefab

---
+++ BYVGameDevTask/Assets/Scripts/Gameplay/Ground.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    public Rigidbody2D[] rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       
+        if (collision.gameObject.CompareTag("body1"))
+        {
+           
+
+            for (int i = 0; i < rb.Length; i++)
+            {
+                
+                //Stop Moving/Translating
+                rb[i].velocity = Vector2.zero;
+                //Stop rotating
+                rb[i].angularVelocity = 0;
+            }
+
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("body1"))
+        {
+
+
+            for (int i = 0; i < rb.Length; i++)
+            {
+
+                //Stop Moving/Translating
+                //rb[i].velocity = Vector2.zero;
+                //Stop rotating
+                rb[i].angularVelocity = 0;
+            }
+
+        }
+    }
+}
--- BYVGameDevTask/Assets/Scripts/Gameplay/PlayerController.cs
+++ BYVGameDevTask/Assets/Scripts/Gameplay/PlayerController.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerJump : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     GameObject player1;
     [SerializeField]
     GameObject player2;
 
+    public GameObject body1;
+    public GameObject body2;
+    public GameObject bullet;
+    public GameObject bulletM;
+    public Transform gunL;
+    public Transform gunR;
+    
     public LayerMask ground;
 
     public float positionRadius;
 
+    public float jumpForce;
+    public float doubleJumpForce;
+    public Vector2 jumpHeight;
     public Transform playerPos1;
     public Transform playerPos2;
 
@@ -23,9 +33,12 @@ public class PlayerJump : MonoBehaviour
 
     Animator animator1;
     Animator animator2;
-    
 
-    
+    private Rigidbody2D rb1;
+    private Rigidbody2D rb2;
+
+
+
     private void Awake()
     {
         Input.multiTouchEnabled = true;
@@ -34,9 +47,22 @@ public class PlayerJump : MonoBehaviour
     {
         animator1 = player1.GetComponent<Animator>();
         animator2 = player2.GetComponent<Animator>();
+        rb1 = body1.GetComponent<Rigidbody2D>();
+        rb2 = body2.GetComponent<Rigidbody2D>();
+
+
+        Collider2D[] colliders = transform.GetComponentsInChildren<Collider2D>();
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            for (int k = i + 1; k < colliders.Length; k++)
+            {
+                Physics2D.IgnoreCollision(colliders[i], colliders[k]);
+            }
+        }
     }
     private void Update()
     {
+
         isGrounded1 = Physics2D.OverlapCircle(playerPos1.position, positionRadius, ground);
         isGrounded2 = Physics2D.OverlapCircle(playerPos2.position, positionRadius, ground);
 
@@ -44,7 +70,6 @@ public class PlayerJump : MonoBehaviour
         {
             animator1.SetBool("CanJump", true);
             canDoubleJump1 = true;
-
         }
         if (isGrounded2)
         {
@@ -55,35 +80,50 @@ public class PlayerJump : MonoBehaviour
     public void JumpL()
     {
 
+
         if (isGrounded1)
         {
-            animator1.enabled = true;
-            animator1.Play("Jump");
+            rb1.velocity = new Vector2(rb1.velocity.x, jumpForce*Time.deltaTime);
+            animator1.SetBool("CanJump", false);
+            animator1.SetTrigger("jump");
+            //rb1.AddForce(Vector2.up * jumpForce*Time.deltaTime, ForceMode2D.Force);
+
         }
         else
         {
             if (canDoubleJump1)
             {
-                
+                //rb1.velocity = new Vector2(rb1.velocity.x, jumpForce*Time.deltaTime);
+                rb1.AddForce(Vector2.up * doubleJumpForce * Time.deltaTime, ForceMode2D.Force);
                 canDoubleJump1 = false;
             }
         }
     }
     public void JumpR()
     {
+       
         if (isGrounded2)
         {
-            animator2.enabled = true;
-            animator2.Play("FixedRagdollJump2");    
+            rb2.velocity = new Vector2(rb2.velocity.x, jumpForce);
+            animator2.SetBool("CanJump", false);
+            animator2.Play("Jump");
         }
         else
         {
             if (canDoubleJump2)
             {
-                
+
                 canDoubleJump2 = false;
             }
         }
-
     }
+    public void ShootL()
+    {
+        Instantiate(bullet, gunL.position, Quaternion.identity);
+    }
+    
+
 }
+
+
+
